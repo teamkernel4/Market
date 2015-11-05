8fe0c706350b4a5433c4f3c2f7fb14423f2c510a
DaeJunLee <dejavudev@gmail.com>
2015-11-05 12:20:24 +0900

    First commit

---
+++ src/com/teamkernel4/market/MainActivity.java
@@ -0,0 +1,57 @@
+package com.teamkernel4.market;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * File : MainActivity.java
+ * INFO : 기본 Activity
+ * USER : LDJ
+ * DATA : 2015. 11. 5.
+ */
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+	}
+
+	@Override
+	protected void onRestart() {
+		// TODO Auto-generated method stub
+		super.onRestart();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+	
+	
+}
